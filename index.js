75c4ef9ed06a2cfd73884fb040ce6baee16e8b48
Andres <andres@bitnami.com>
2016-09-23 20:15:19 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,287 @@
+'use strict';
+
+const _ = require('nami-utils/lodash-extra');
+const path = require('path');
+const spawnSync = require('child_process').spawnSync;
+const strftime = require('strftime');
+const nfile = require('nami-utils').file;
+const nos = require('nami-utils').os;
+const logExec = require('common-utils').logExec;
+
+function _getDummyLogger() {
+  return {info: console.log, error: console.error, debug: function() {}};
+}
+
+/**
+ * Execute command using docker binary
+ * @function exec
+ * @param {string|Array} command - Command to execute
+ * @param {Object} [options] - Options passed to runProgram method
+ * @example
+ * exec('cp', ['test:/tmp/log', '/tmp/log']);
+ */
+function exec(command, options) {
+  return nos.runProgram('docker', command, options);
+}
+
+function _removeContainer(containerId, callback, options) {
+  options = _.opts(options, {force: false, logger: _getDummyLogger()});
+  const logger = options.logger;
+  try {
+    exec(`rm ${options.force ? '-f' : ''} ${containerId}`);
+    logger.debug('Temporary container successfully deleted');
+  } catch (e) {
+    logger.error(`Failed to delete container ${e}`);
+  }
+  if (callback) {
+    callback();
+  }
+}
+
+function _getBinds(mappings) {
+  const result = [];
+  _.each(mappings, function(containerPath, hostPath) {
+    let mode = null;
+    if (_.isString(containerPath) || _.isReallyObject(containerPath)) {
+      if (_.isReallyObject(containerPath)) {
+        mode = containerPath.mode;
+        containerPath = containerPath.path;
+      }
+    } else {
+      throw new Error(`Invalid mapping spec ${JSON.stringify(containerPath)}`);
+    }
+    let bindSpec = `${hostPath}:${containerPath}`;
+    if (mode) bindSpec += `:${mode}`;
+    result.push('-v', bindSpec);
+  });
+  return result;
+}
+
+function _parseRunOptions(runOptions) {
+  const result = [];
+  _.each(runOptions, (value, key) => {
+    if (value !== false) result.push(`--${key}`);
+    if (!_.isNull(value) && !_.isUndefined(value) && value !== true) result.push(value);
+  });
+  return result;
+}
+/**
+ * Opens a bash shell in a docker image
+ * @function shell
+ * @param {string} imageId - Container image id
+ * @param {Object} [options]
+ * @param {string} [options.root] - Path to overlay with the container root directory
+ * @param {Object} [options.mappings] - Key-value with the volumes to map. The key should be the host path and its
+ * value should be a string for the container path or an object specifying the path and the access mode (rw, ro)
+ * @param {Object} [options.runOptions] - Key-value with the docker run command options to use. The key should be
+ * the full name of the option and the value should be a string or a boolean if it doesn't have a value
+ * @example
+ * shell('centos', {
+ * root: '/tmp/container-root/',
+ * }, {
+ *   mappings: {
+ *     '/tmp/test': '/tmp/test',
+ *     '/tmp/read': {path: '/tmp/read', mod: 'ro'}
+ * }, {
+ *   runOptions: {
+ *     'name': 'my-container',
+ *     'privileged': true
+ * });
+ */
+function shell(imageId, options) {
+  options = _.opts(options, {root: null, mappings: {}, runOptions: {}});
+  options.runOptions = _.opts(options.runOptions, {
+    'interactive': true,
+    'tty': true,
+  });
+  const runOptions = _parseRunOptions(options.runOptions);
+  const mappings = options.mappings;
+  if (options.root) {
+    _.each(nfile.glob(`${options.root}/*`), function(f) { // If root is given map every directory inside of it
+      mappings[f] = path.join('/', path.basename(f));
+    });
+  }
+  const binds = _getBinds(mappings);
+  const cmdArgs = ['run'].concat(runOptions, binds);
+  cmdArgs.push(imageId, 'bash');
+  return spawnSync('docker', cmdArgs, {stdio: 'inherit'});
+}
+
+/**
+ * Obtains an image id from a image name
+ * @function getImageId
+ * @param {string} imageName - Name of the image
+ * @returns {string} - Image id
+ * @example
+ * getImageId('centos');
+ * // => 'a8de57517228'
+ */
+function getImageId(imageName) {
+  const res = exec(`images -q ${imageName}`).split('\n')[0];
+  return !_.isEmpty(res) ? res : null;
+}
+
+/**
+ * Obtains a container id from its name
+ * @function getContainerId
+ * @param {string} name - Name of the container
+ * @returns {string} - Container ID
+ * @example
+ * getContainerId('silly_cori');
+ * // => '92b09381df3f'
+ */
+function getContainerId(name) {
+  const res = exec(`ps -aq --filter "name=${name}"`).replace('\n', '');
+  return !_.isEmpty(res) ? res : null;
+}
+/**
+ * Run a command in a Docker image
+ * @function runInContainer
+ * @param {string} id - Container image id
+ * @param {string|Array} cmd - Command to execute
+ * @param {Function} callback(container, err, data) - Callback to execute after running the command
+ * @param {Object} [options]
+ * @param {Object} [options.mappings] - Key-value with the volumes to map using the host path as key
+ * and a string for the container path or an object specifying the path and the access mode (rw, ro)
+ * @param {Object} [options.logger] - Logger to use
+ * @param {Object} [options.timeout=10800] - Timeout for the command to run. Three hours by default.
+ * Infinity is a valid value
+ * @param {Object} [options.exitOnEnd=true] - Exit the current process after finishing
+ * @example
+ * runInContainer('centos', ['find', '/'], () => {
+ *   console.log('Command finished');
+ * }, {
+ *   mappings: {
+ *     '/tmp/test': '/tmp/test',
+ *     '/tmp/read': {path: '/tmp/read', mod: 'ro'}
+ * });
+ */
+function runInContainer(id, cmd, callback, options) {
+  options = _.opts(options, {mappings: [], logger: _getDummyLogger(), timeout: 10800, runOptions: {}, exitOnEnd: true});
+  options.runOptions = _.opts(options.runOptions, {
+    'interactive': null,
+    'name': strftime('%s')
+  });
+  const runOptions = _parseRunOptions(options.runOptions);
+  const binds = _getBinds(options.mappings);
+  const _parseMsg = function(msg) {
+    // Avoid multiple '\n'
+    msg = _.compact(msg.toString('utf8').split('\n'));
+    return msg;
+  };
+  const onStdout = function(msg) {
+    _.each(_parseMsg(msg), line => {
+      options.logger.debug(`[docker] ${line}`);
+    });
+  };
+  const onStderr = function(msg) {
+    _.each(_parseMsg(msg), line => {
+      options.logger.error(`[docker] ${line}`);
+    });
+  };
+  const onExit = function(res) {
+    const containerId = getContainerId(options.runOptions.name);
+    if (callback) callback({id: containerId}, res);
+    _removeContainer(containerId, () => {
+      if (options.exitOnEnd) process.exit();
+    }, {logger: options.logger, force: true});
+  };
+  process.on('SIGINT', function() {
+    onExit({exitCode: 127});
+  });
+  const _cmd = ['docker', 'run'].concat(binds, runOptions, id, cmd);
+  const handler = nos.execAsync(_cmd.join(' '), {onStdout, onStderr, onExit});
+  try {
+    handler.wait({timeout: options.timeout, throwOnTimeout: true});
+  } catch (e) {
+    const containerId = getContainerId(options.runOptions.name);
+    _removeContainer(containerId, null, {force: true});
+    throw e;
+  }
+}
+
+/**
+ * Pull an image using docker binary
+ * @function pull
+ * @param {string} image - Image to pull
+ * @example
+ * pull('centos');
+ */
+function pull(image) {
+  return exec(`pull ${image}`);
+}
+
+/**
+ * Check if an image exists
+ * @function imageExists
+ * @param {string} imageName - Name of the image
+ * @returns {boolean}
+ * @example
+ * imageExists('centos');
+ * // => true
+ */
+function imageExists(imageName) {
+  return !_.isEmpty(getImageId(imageName));
+}
+
+/**
+ * Load a docker image from a tar or a tar.gz
+ * @function imageExists
+ * @param {string} imagePath - Path to the tarball
+ * @example
+ * loadImage('/tmp/centos.tar.gz');
+ */
+function loadImage(imagePath) {
+  if (!nfile.exists(imagePath)) {
+    throw new Error(`Cannot load ${imagePath} as docker image: file does not exist.`);
+  }
+  if (path.extname(imagePath) === '.gz') {
+    logExec('gunzip', imagePath);
+    imagePath = imagePath.replace('.gz', '');
+  }
+  return exec(`load -i ${imagePath}`);
+}
+
+/**
+ * Build a docker image
+ * @function build
+ * @param {string} imagePath - Path to the Dockerfile directory
+ * @param {string} name - Name of the image
+ * @param {Object} [options]
+ * @param {string} [options.tag='latest'] - Tag of the image
+ * @example
+ * build('/tmp/centos/', 'centos', {tag: 'r01'});
+ */
+function build(imagePath, name, options) {
+  options = _.opts(options, {tag: 'latest'});
+  return exec(`build -t ${name}:${options.tag} ${imagePath}`);
+}
+
+/**
+ * Check if Docker daemon is available and running
+ * @function verifyConnection
+ * @return {boolean}
+ */
+function verifyConnection() {
+  let res = false;
+  try {
+    exec('ps');
+    res = true;
+  } catch (e) {
+    throw new Error(`Docker is not available: \n${e.message}`);
+  }
+  return res;
+}
+
+module.exports = {
+  runInContainer,
+  shell,
+  exec,
+  pull,
+  loadImage,
+  imageExists,
+  getImageId,
+  getContainerId,
+  build,
+  verifyConnection
+};
---
+++ test/index.js
@@ -0,0 +1,173 @@
+'use strict';
+/* eslint-disable no-unused-expressions */
+const chai = require('chai');
+const expect = chai.expect;
+const spawnSync = require('child_process').spawnSync;
+const fs = require('fs');
+const path = require('path');
+
+const du = require('../index.js');
+
+const testDir = '/tmp/compilation-utils-tests';
+
+describe('Docker utils', function() {
+  let isDockerAvalilable = false;
+  this.timeout(5000);
+  try {
+    isDockerAvalilable = du.verifyConnection();
+  } catch (e) { /* Not available */ }
+  it('docker should be available', () => {
+    expect(isDockerAvalilable).to.be.eql(true);
+  });
+  if (isDockerAvalilable) {
+    describe('#exec()', () => {
+      it('executes a command', () => {
+        const res = du.exec('ps');
+        expect(res).to.contain('CONTAINER ID');
+      });
+      it('executes a command with options', () => {
+        const res = du.exec('ps', {retrieveStdStreams: true});
+        expect(res.stdout).to.contain('CONTAINER ID');
+        expect(res.code).to.eql(0);
+      });
+    });
+    describe('#shell()', () => {
+      const previousPATH = process.env.PATH;
+      beforeEach(() => {
+        spawnSync('rm', ['-rf', testDir]);
+        fs.mkdirSync(testDir);
+        fs.writeFileSync(path.join(testDir, 'docker'), `#!/bin/bash\ntrue`, {mode: '0755'});
+        process.env.PATH = `${testDir}:${process.env.PATH}`; // Mocks docker binary
+      });
+      afterEach(() => {
+        spawnSync('rm', ['-rf', testDir]);
+        process.env.PATH = previousPATH;
+      });
+      it('should open a shell', () => {
+        const res = du.shell('test-image');
+        expect(res.args).to.be.eql(['docker', 'run', '--interactive', '--tty', 'test-image', 'bash']);
+      });
+      it('should mount root', () => {
+        fs.mkdirSync(path.join(testDir, 'root'));
+        fs.mkdirSync(path.join(testDir, 'root/test'));
+        fs.mkdirSync(path.join(testDir, 'root/test/1'));
+        const res = du.shell('test-image', {root: path.join(testDir, 'root/test')});
+        expect(res.args).to.be.eql([
+          'docker', 'run', '--interactive', '--tty',
+          '-v', `${path.join(testDir, 'root/test/1')}:/1`, 'test-image', 'bash'
+        ]);
+      });
+      it('should map a directory', () => {
+        fs.mkdirSync(path.join(testDir, 'test'));
+        const mappings = {};
+        mappings[path.join(testDir, 'test')] = '/tmp/test';
+        const res = du.shell('test-image', {mappings});
+        expect(res.args).to.be.eql([
+          'docker', 'run', '--interactive', '--tty',
+          '-v', `${path.join(testDir, 'test')}:/tmp/test`, 'test-image', 'bash'
+        ]);
+      });
+      it('should parse run options', () => {
+        fs.mkdirSync(path.join(testDir, 'test'));
+        const mappings = {};
+        mappings[path.join(testDir, 'test')] = '/tmp/test';
+        const res = du.shell('test-image', {runOptions: {name: 'test', privileged: true}});
+        expect(res.args).to.be.eql([
+          'docker', 'run', '--name', 'test', '--privileged', '--interactive', '--tty', 'test-image', 'bash'
+        ]);
+      });
+    });
+    describe('#getImageId', () => {
+      after(() => {
+        du.exec('rmi hello-world:test');
+      });
+      it('loads and gets an image ID', () => {
+        du.loadImage(path.join(__dirname, 'resources/base-image.tar'));
+        const id = du.getImageId('hello-world:test');
+        expect(id).to.not.be.empty;
+      });
+    });
+    describe('#getContainerId', () => {
+      after(() => {
+        du.exec('rm -f docker-utils-test');
+        du.exec('rmi hello-world:test');
+      });
+      it('loads and gets a container ID', () => {
+        du.loadImage(path.join(__dirname, 'resources/base-image.tar'));
+        du.exec('run --name docker-utils-test hello-world:test');
+        expect(du.getContainerId('docker-utils-test')).to.not.be.empty;
+      });
+    });
+    describe('#imageExists', () => {
+      after(() => {
+        du.exec('rmi hello-world:test');
+      });
+      it('loads and find an image', () => {
+        du.loadImage(path.join(__dirname, 'resources/base-image.tar'));
+        const res = du.imageExists('hello-world:test');
+        expect(res).to.be.true;
+      });
+    });
+    describe('#build', () => {
+      after(() => {
+        du.exec('rmi hello-world:test');
+        du.exec('rmi hello-world:test-2');
+      });
+      it('builds a new image', () => {
+        du.loadImage(path.join(__dirname, 'resources/base-image.tar'));
+        fs.mkdirSync(testDir);
+        fs.writeFileSync(path.join(testDir, 'Dockerfile'), `FROM hello-world:test\n`);
+        du.build(testDir, 'hello-world', {tag: 'test-2'});
+        const res = du.imageExists('hello-world:test-2');
+        expect(res).to.be.true;
+      });
+    });
+    describe('#runInContainer()', function() {
+      const previousPATH = process.env.PATH;
+      this.timeout(10000);
+      beforeEach(() => {
+        spawnSync('rm', ['-rf', testDir]);
+        fs.mkdirSync(testDir);
+        fs.writeFileSync(path.join(testDir, 'docker'), `#!/bin/bash\nsleep 2\necho "$@"`, {mode: '0755'});
+        process.env.PATH = `${testDir}:${process.env.PATH}`; // Mocks docker binary
+      });
+      afterEach(() => {
+        spawnSync('rm', ['-rf', testDir]);
+        process.env.PATH = previousPATH;
+      });
+      it('runs a command', () => {
+        let finished = false;
+        const callback = () => {
+          finished = true;
+        };
+        let res = '';
+        const write = (text) => res += `${text}\n`;
+        du.runInContainer('test-image', 'true', callback, {
+          runOptions: {name: 'docker-utils-test'},
+          mappings: {
+            '/tmp/test': {path: '/container/tmp/test', mode: 'rw'}
+          },
+          exitOnEnd: false,
+          logger: {
+            info: write,
+            debug: write,
+            error: write
+          }
+        });
+        expect(finished).to.be.true;
+        expect(res).to.contain('run -v /tmp/test:/container/tmp/test:rw --name docker-utils-test ' +
+        '--interactive test-image true');
+      });
+      it('throws an error on timeout', () => {
+        const logger = {
+          info: () => {},
+          debug: () => {},
+          error: () => {}
+        };
+        expect(() => {
+          du.runInContainer('test-image', 'true', null, {timeout: 1, exitOnEnd: false, logger});
+        }).to.throw('Exceeded timeout');
+      });
+    });
+  }
+});
